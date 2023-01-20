8f726505c5912d5598056af3433a2e936be2a802
orall <1140777300@qq.com>
2023-01-20 14:43:57 +0800

    * 更新：更新了统一返回格式（待完善）

--- controller/userController.go
+++ controller/userController.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"go_douyin/model"
 	"go_douyin/service"
+	"go_douyin/utils/response"
 	"net/http"
 )
 
@@ -19,7 +20,10 @@ func (h *UserController) Register(c *gin.Context) {
 	var user model.User
 	c.BindJSON(&user)
 	if h.UserService.Register(user) {
-		c.JSON(http.StatusOK, gin.H{"code": "200", "msg": "OK", "data": "注册成功"})
+		response.Success(c, "注册成功", gin.H{
+			"id": '1',
+		}, "token")
+		//c.JSON(http.StatusOK, gin.H{"code": "200", "msg": "OK", "data": "注册成功"})
 	} else {
 		c.JSON(http.StatusBadRequest, gin.H{"code": "500", "msg": "OK", "data": "注册失败"})
 	}
@@ -29,7 +33,9 @@ func (h *UserController) Login(c *gin.Context) {
 	var user model.User
 	c.BindJSON(&user)
 	if h.UserService.Login(user.Username, user.Password) {
-		c.JSON(http.StatusOK, gin.H{"code": "200", "msg": "OK", "data": "登录成功"})
+		response.Success(c, "登录成功", gin.H{
+			"id": '1',
+		}, "token")
 	} else {
 		c.JSON(http.StatusBadRequest, gin.H{"code": "500", "msg": "OK", "data": "登录失败"})
 	}
