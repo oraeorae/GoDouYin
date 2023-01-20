ceb78f86b8821411d769d48e3cfdd2e544f54c49
orall <1140777300@qq.com>
2023-01-20 15:55:49 +0800

    * 修复：修复了统一返回格式只能嵌套的问题

---
+++ utils/response/response.go
@@ -0,0 +1,92 @@
+package response
+
+import (
+	"github.com/gin-gonic/gin"
+	"go_douyin/global"
+	"net/http"
+)
+
+func ReturnJson(Context *gin.Context, httpCode int, dataCode int, msg string, data interface{}) {
+	//Context.Header("key2020","value2020")  	//可以根据实际情况在头部添加额外的其他信息
+	response := gin.H{
+		//状态码，0成功，其他值失败
+		"status_code": dataCode,
+		//返回状态描述
+		"status_msg": msg,
+	}
+	// 这种写法确保是平级，而不是嵌套
+	//if dataMap, ok := data.(map[string]interface{}); ok {
+	//	for k, v := range dataMap {
+	//		response[k] = v
+	//	}
+	//}
+	// gin.H的直接加入
+	for k, v := range data.(gin.H) {
+		response[k] = v
+	}
+	Context.JSON(httpCode, response)
+}
+
+//ReturnJsonFromString 将json字符窜以标准json格式返回（例如，从redis读取json格式的字符串，返回给浏览器json格式）
+func ReturnJsonFromString(Context *gin.Context, httpCode int, jsonStr string) {
+	Context.Header("Content-Type", "application/json; charset=utf-8")
+	Context.String(httpCode, jsonStr)
+}
+
+// 语法糖函数封装
+
+//Success 直接返回成功
+func Success(c *gin.Context, msg string, data interface{}) {
+	ReturnJson(c, http.StatusOK, global.CurdStatusOkCode, msg, data)
+}
+
+//Fail 失败的业务逻辑
+func Fail(c *gin.Context, dataCode int, msg string, data interface{}) {
+	ReturnJson(c, http.StatusBadRequest, dataCode, msg, data)
+	c.Abort()
+}
+
+// ErrorTokenBaseInfo token 基本的格式错误
+func ErrorTokenBaseInfo(c *gin.Context) {
+	ReturnJson(c, http.StatusBadRequest, http.StatusBadRequest, global.ErrorsTokenBaseInfo, "")
+	//终止可能已经被加载的其他回调函数的执行
+	c.Abort()
+}
+
+//ErrorTokenAuthFail token 权限校验失败
+func ErrorTokenAuthFail(c *gin.Context) {
+	ReturnJson(c, http.StatusUnauthorized, http.StatusUnauthorized, global.ErrorsNoAuthorization, "")
+	//终止可能已经被加载的其他回调函数的执行
+	c.Abort()
+}
+
+//ErrorTokenRefreshFail token不符合刷新条件
+func ErrorTokenRefreshFail(c *gin.Context) {
+	ReturnJson(c, http.StatusUnauthorized, http.StatusUnauthorized, global.ErrorsRefreshTokenFail, "")
+	//终止可能已经被加载的其他回调函数的执行
+	c.Abort()
+}
+
+//token 参数校验错误
+func TokenErrorParam(c *gin.Context, wrongParam interface{}) {
+	ReturnJson(c, http.StatusUnauthorized, global.ValidatorParamsCheckFailCode, global.ValidatorParamsCheckFailMsg, wrongParam)
+	c.Abort()
+}
+
+// ErrorCasbinAuthFail 鉴权失败，返回 405 方法不允许访问
+func ErrorCasbinAuthFail(c *gin.Context, msg interface{}) {
+	ReturnJson(c, http.StatusMethodNotAllowed, http.StatusMethodNotAllowed, global.ErrorsCasbinNoAuthorization, msg)
+	c.Abort()
+}
+
+//ErrorParam 参数校验错误
+func ErrorParam(c *gin.Context, wrongParam interface{}) {
+	ReturnJson(c, http.StatusBadRequest, global.ValidatorParamsCheckFailCode, global.ValidatorParamsCheckFailMsg, wrongParam)
+	c.Abort()
+}
+
+// ErrorSystem 系统执行代码错误
+func ErrorSystem(c *gin.Context, msg string, data interface{}) {
+	ReturnJson(c, http.StatusInternalServerError, global.ServerOccurredErrorCode, global.ServerOccurredErrorMsg+msg, data)
+	c.Abort()
+}
